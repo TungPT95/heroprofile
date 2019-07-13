c7ca61919d9a8fa90a658cde6f970a6d2dea7f36
tungpham <phamthanhtung1995@gmail.com>
2019-07-13 23:32:29 +0700

    # add comic_movie_repos.dart

---
+++ lib/models/comic_movie.dart
@@ -0,0 +1,9 @@
+import 'movie.dart';
+
+class ComicMovie {
+  int id;
+  String comicTitle;
+  List<Movie> movies;
+
+  ComicMovie({this.id, this.comicTitle, this.movies});
+}
---
+++ lib/models/movie.dart
@@ -0,0 +1,7 @@
+class Movie {
+  int id;
+  String title;
+  String desc;
+
+  Movie({this.id, this.title, this.desc});
+}
