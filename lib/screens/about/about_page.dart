976e8c1dbb4c3a404d25c16eaec3699affc96279
tungpham <phamthanhtung1995@gmail.com>
2019-08-19 23:06:17 +0700

    scale size

--- lib/screens/about/about_page.dart
+++ lib/screens/about/about_page.dart
@@ -33,17 +33,22 @@ class _AboutPageState extends BasePageState<AboutPage> {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: ListView(
-        padding: EdgeInsets.only(left: 8, right: 8, bottom: 100, top: 66),
+        padding: EdgeInsets.only(
+            left: scaleWidth(8),
+            right: scaleWidth(8),
+            bottom: scaleHeight(88 + 10.0),
+            top: scaleHeight(56 + 10.0)),
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(left: 4, right: 4),
+            padding: EdgeInsets.symmetric(horizontal: scaleWidth(4)),
             child: CustomizeCard(
-              contentPadding: EdgeInsets.all(8),
+              contentPadding: EdgeInsets.symmetric(
+                  horizontal: scaleWidth(12), vertical: scaleHeight(12)),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.only(right: 8),
+                    padding: EdgeInsets.only(right: scaleWidth(8)),
                     child: Material(
                       type: MaterialType.card,
                       clipBehavior: Clip.antiAlias,
@@ -53,8 +58,8 @@ class _AboutPageState extends BasePageState<AboutPage> {
                         't1.0-9/26230311_1452700444852318_4211563881416746955_n.jpg?'
                         '_nc_cat=105&_nc_oc=AQmAnzc46n7nRPN6Z09HzK1zq7Dak9NodoVRn0vB6-Za2wNM31tiDTQBcQps3Nn4isaog89wsSjpnSnWyd_'
                         'REF6K&_nc_ht=scontent.fsgn5-2.fna&oh=0c00c0a3aadd8af7611c989341e89d32&oe=5D8B3D49',
-                        width: 80,
-                        height: 80,
+                        width: scaleWidth(80),
+                        height: scaleHeight(80),
                       ),
                     ),
                   ),
@@ -65,22 +70,25 @@ class _AboutPageState extends BasePageState<AboutPage> {
                       children: <Widget>[
                         Text(
                           'Tung Pham',
+                          textScaleFactor: screenWidthRatio(),
                           style: TextStyle(
                               color: Colors.black,
                               fontSize: 20,
                               fontWeight: FontWeight.bold),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(top: 4),
+                          padding: EdgeInsets.only(top: scaleHeight(4)),
                           child: Text(
                             'Jan 06, 1995',
+                            textScaleFactor: screenWidthRatio(),
                             style: TextStyle(color: Colors.black),
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.only(top: 12),
+                          padding: EdgeInsets.only(top: scaleHeight(12)),
                           child: Text(
                             '\"I\'m Flutter, Android Developer. I want to have more and more experiences in mobile development\"',
+                            textScaleFactor: screenWidthRatio(),
                             style: TextStyle(color: Colors.black),
                           ),
                         ),
@@ -95,35 +103,49 @@ class _AboutPageState extends BasePageState<AboutPage> {
             onTap: () => _launchUrl('tel:+84 394 954 959'),
             content: '+84-39-495-4959',
             icon: 'assets/images/phone.png',
+            heightRatio: screenHeightRatio(),
+            widthRatio: screenWidthRatio(),
           ),
           InfoCard(
-            onTap: () =>_launchUrl('mailto:phamthanhtung1995@gmail.com'),
+            onTap: () => _launchUrl('mailto:phamthanhtung1995@gmail.com'),
             content: 'phamthanhtung1995@gmail.com',
             icon: 'assets/images/gmail.png',
+            heightRatio: screenHeightRatio(),
+            widthRatio: screenWidthRatio(),
           ),
           InfoCard(
             onTap: () => _launchUrl('https://facebook.com/tungpt.95'),
             content: 'facebook.com/tungpt.95',
             icon: 'assets/images/facebook.png',
+            heightRatio: screenHeightRatio(),
+            widthRatio: screenWidthRatio(),
           ),
           InfoCard(
             onTap: () => _launchUrl('https://linkedin.com/in/tungpt95'),
             content: 'linkedin.com/in/tungpt95',
             icon: 'assets/images/linkedin.png',
+            heightRatio: screenHeightRatio(),
+            widthRatio: screenWidthRatio(),
           ),
           InfoCard(
             onTap: () {},
             content: 'live:phamthanhtung1995',
             icon: 'assets/images/skype.png',
+            heightRatio: screenHeightRatio(),
+            widthRatio: screenWidthRatio(),
           ),
           InfoCard(
             onTap: () => _launchUrl('https://github.com/tungpham6195'),
             content: 'github.com/tungpham6195',
             icon: 'assets/images/github.png',
+            heightRatio: screenHeightRatio(),
+            widthRatio: screenWidthRatio(),
           ),
           InfoCard(
             content: 'Version: $_versionName',
             icon: 'assets/images/ic_version.png',
+            heightRatio: screenHeightRatio(),
+            widthRatio: screenWidthRatio(),
           ),
         ],
       ),
--- lib/screens/about/info_card.dart
+++ lib/screens/about/info_card.dart
@@ -6,26 +6,36 @@ class InfoCard extends StatelessWidget {
   final OnTapCallback onTap;
   final String content;
   final String icon;
+  final double widthRatio;
+  final double heightRatio;
 
-  InfoCard({this.content = "", this.icon = "", this.onTap})
+  InfoCard(
+      {this.content = "",
+      this.icon = "",
+      this.onTap,
+      this.widthRatio = 1,
+      this.heightRatio = 1})
       : assert(content != null),
         assert(icon != null);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: 15, left: 4, right: 4),
+      padding: EdgeInsets.only(
+          top: 15 * heightRatio, left: 4 * widthRatio, right: 4 * widthRatio),
       child: CustomizeCard(
         onTap: onTap,
-        contentPadding: const EdgeInsets.all(8.0),
+        contentPadding: EdgeInsets.symmetric(
+            horizontal: 8 * widthRatio, vertical: 12 * heightRatio),
         child: Row(
           children: <Widget>[
-            Image.asset(icon, width: 24, height: 24),
+            Image.asset(icon, width: 24 * widthRatio, height: 24 * heightRatio),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
+                padding: EdgeInsets.only(left: 8.0 * widthRatio),
                 child: ThemedText(
                   content,
+                  textScaleFactor: widthRatio,
                 ),
               ),
             )
